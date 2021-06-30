d19237e9d68b41c39b5a6e899afe5236cd19556e
Fitz-G <wilfried.korandji@gmail.com>
2021-06-30 12:21:59 +0000

    Home page template integration Ok

---
+++ resources/views/includes/sidebar.blade.php
@@ -0,0 +1,70 @@
+<div class="sidebar-wrapper" data-simplebar="true">
+    <div class="sidebar-header">
+        <div>
+            <img src="assets/images/logo-icon.png" class="logo-icon" alt="logo icon">
+        </div>
+        <div>
+            <h4 class="logo-text">Rocker</h4>
+        </div>
+        <div class="toggle-icon ms-auto"><i class='bx bx-arrow-to-left'></i>
+        </div>
+    </div>
+    <!--navigation-->
+    <ul class="metismenu" id="menu">
+        <li>
+            <a href="{{ route('home') }}">
+                <div class="parent-icon"><i class='bx bx-home-circle'></i>
+                </div>
+                <div class="menu-title">Tableau de board</div>
+            </a>
+        </li>
+        <li class="menu-label">Gestion utilisateurs</li>
+        <li>
+            <a href="javascript:;" class="has-arrow">
+                <div class="parent-icon"><i class='bx bx-user-circle'></i>
+                </div>
+                <div class="menu-title">Users</div>
+            </a>
+            <ul>
+                <li>
+                    <a href="#"><i class="bx bx-right-arrow-alt"></i>Créer un User</a>
+                </li>
+                <li>
+                    <a href="#"><i class="bx bx-right-arrow-alt"></i>Liste des Users</a>
+                </li>
+            </ul>
+        </li>
+        <li class="menu-label">Configuration</li>
+        <li>
+            <a href="javascript:;" class="has-arrow">
+                <div class="parent-icon"><i class='bx bx-user-check'></i>
+                </div>
+                <div class="menu-title">Rôles</div>
+            </a>
+            <ul>
+                <li>
+                    <a href="#"><i class="bx bx-right-arrow-alt"></i>Créer un rôle</a>
+                </li>
+                <li>
+                    <a href="#"><i class="bx bx-right-arrow-alt"></i>Liste des rôles</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="javascript:;" class="has-arrow">
+                <div class="parent-icon"><i class='bx bx-user-x'></i>
+                </div>
+                <div class="menu-title">Permissions</div>
+            </a>
+            <ul>
+                <li>
+                    <a href="#"><i class="bx bx-right-arrow-alt"></i>Créer une permission</a>
+                </li>
+                <li>
+                    <a href="#"><i class="bx bx-right-arrow-alt"></i>Liste des permissions</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+    <!--end navigation-->
+</div>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--favicon-->
+    <link rel="icon" href="assets/images/favicon-32x32.png" type="image/png" />
+    <!--plugins-->
+    <link href="{{ asset('assets/plugins/vectormap/jquery-jvectormap-2.0.2.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/plugins/simplebar/css/simplebar.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/plugins/metismenu/css/metisMenu.min.css') }}" rel="stylesheet" />
+    <!-- loader-->
+    <link href="{{ asset('assets/css/pace.min.css') }}" rel="stylesheet" />
+    <script src="{{ asset('assets/js/pace.min.js') }}"></script>
+    <!-- Bootstrap CSS -->
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+    <link href="{{ asset('assets/css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/icons.css') }}" rel="stylesheet">
+    <!-- Theme Style CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/dark-theme.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/semi-dark.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/header-colors.css') }}" />
+    <title>@yield('title')</title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <!--sidebar wrapper -->
+        @include('includes.sidebar')
+        <!--end sidebar wrapper -->
+
+        <!--start header -->
+        @include('includes.header')
+        <!--end header -->
+
+        <div class="page-wrapper">
+            <div class="page-content-wrapper">
+                <div class="page-content">
+                    @yield('content')
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Bootstrap JS -->
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+    <!--plugins-->
+    <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/simplebar/js/simplebar.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/metismenu/js/metisMenu.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js') }}"></script>
+    <script src="{{ asset('assets/plugins/vectormap/jquery-jvectormap-2.0.2.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/vectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
+    <script src="{{ asset('assets/plugins/chartjs/js/Chart.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/chartjs/js/Chart.extension.js') }}"></script>
+    <script src="{{ asset('assets/js/index.js') }}"></script>
+    <!--app JS-->
+    <script src="{{ asset('assets/js/app.js') }}"></script>
+</body>
+
+</html>
